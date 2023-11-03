dad643ddf70006b3f114676b6e52590c02d2d5aa
Mohandkajuluri <md.kajuluri@gmail.com>
2023-11-03 13:38:48 -0400

    echo n color test

--- sample.sh
+++ sample.sh
@@ -1,6 +1,11 @@
-echo "Hello world"
+
 echo Hello world
 
 #color syntax
 
 echo -e "\e[31m This text is in RED color \e[0m"
+echo -e "\e[32m This text is in GREEN color \e[0m"
+echo -e "\e[33m This text is in YELLOW color \e[0m"
+echo -e "\e[34m This text is in BLUE color \e[0m"
+echo -e "\e[35m This text is in MAGENTA color \e[0m"
+echo -e "\e[36m This text is in CYAN color \e[0m"
