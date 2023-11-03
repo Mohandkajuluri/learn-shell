188e4a9f92511348ab48ee9b4ec29d7d657dcfe3
Mohandkajuluri <md.kajuluri@gmail.com>
2023-11-03 13:40:21 -0400

    echo n color test

--- sample.sh
+++ sample.sh
@@ -9,3 +9,5 @@ echo -e "\e[33m This text is in YELLOW color \e[0m"
 echo -e "\e[34m This text is in BLUE color \e[0m"
 echo -e "\e[35m This text is in MAGENTA color \e[0m"
 echo -e "\e[36m This text is in CYAN color \e[0m"
+
+echo -e "\e[33m Hello World \e[0m"
